b1dd6a30964d3f15694f26c86c2e40ac4299a796
valentina <simonova.valentina19@gmail.com>
2024-11-17 12:09:57 +0300

    решение

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -3,8 +3,12 @@ package org.example;
 
 import lombok.SneakyThrows;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class HomeWork {
 
@@ -14,9 +18,35 @@ public class HomeWork {
      */
     @SneakyThrows
     public void championship(InputStream in, OutputStream out) {
+        RedBlackTree<Integer> readBlackTree = new RedBlackTree<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+        String[] firstLine = reader.readLine().split(" ");
+
+        int n = Integer.parseInt(firstLine[0]);
+        int m = Integer.parseInt(firstLine[1]);
+
+        for(int i = 1; i < n + 1; i++) {
+            readBlackTree.add(i);
+        }
+
+        for(int i = 0; i < m; i++) {
+            int[] battleResult = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            readBlackTree.setWinnerBy(
+                    battleResult[2],
+                    IntStream.range(battleResult[0], battleResult[1] + 1).boxed().toArray(Integer[]::new)
+            );
+        }
+
+        for(int i = 1; i < n + 1; i++) {
+            Integer winnerBy = readBlackTree.getWinnerBy(i);
+            out.write(String.valueOf(winnerBy != null ? winnerBy : 0).getBytes());
+            if(i < n) {
+                out.write(" ".getBytes());
+            }
+        }
 
     }
-
 
 
 }
